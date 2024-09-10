61440c99afc6e6cf20f95eb85a0d7d37fe0e16b7
scott wegmann <wegmanns@gmatc.matc.edu>
2024-09-10 15:09:13 -0500

    add validation

--- server.js
+++ server.js
@@ -22,8 +22,12 @@ app.post('/api/users',[
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
 ], (req, res)=> {
-    console.log(req.body);
-    res.send(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    } else {
+      return res.send(req.body);
+    }
 });
 
 // Connection listener
