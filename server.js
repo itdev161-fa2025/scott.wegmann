1d359ada6f795630baa993080b0084be3f63007b
scott wegmann <wegmanns@gmatc.matc.edu>
2024-09-17 15:50:52 -0500

    Completed Activity 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
 //Initialize express application
 const app = express();
@@ -10,6 +11,8 @@ connectDatabase();
 
 // configure Middleware
 app.use(express.json({extended: false}));
+app.use(cors({ origin: 'http://localhost:3000'}));
+
 
 //API endpoints
 app.get('/', (req, res) => 
@@ -31,4 +34,5 @@ app.post('/api/users',[
 });
 
 // Connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
