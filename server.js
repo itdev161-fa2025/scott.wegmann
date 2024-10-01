14f65d725579485a0f8c5425841bb8b9589fb2a0
scott wegmann <wegmanns@gmatc.matc.edu>
2024-10-01 15:10:21 -0500

    completed Activity 5

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import connectDatabase from './config/db';
-import { check, validationResult } from 'express-validator';
+import { buildCheckFunction, check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'cors';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import user from './models/User';
 
 //Initialize express application
 const app = express();
@@ -19,17 +23,68 @@ app.get('/', (req, res) =>
 res.send('http get request sent to root api endpoint')
 );
 
+/**
+*@route Post api/users
+*@desc Register user
+*/
+
 // register user
 app.post('/api/users',[
     check('name', 'Please enter your name').not().isEmpty,
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
-], (req, res)=> {
+], 
+async(req, res)=> {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     } else {
-      return res.send(req.body);
+      const { name, email, password } = req.body;
+      try {
+        //check if user exists
+        let user = await User.findOne({email: email });
+        if (user) {
+          return res 
+          .status(400)
+          .json({errors:[{ msg: 'user already exists'}]});
+        }
+
+        user = new User({
+          name: name,
+          email: email,
+          password: password
+        });
+
+        //Encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        //save to the DB and return 
+        await user.save();
+
+        // generate and return a JWT token 
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
+
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { epiresIn: '10hr'},
+          (err, token) => {
+            if (err) throw err;
+            res.json({token: token});
+          }
+        )
+
+
+
+        res.send('User successfully registered')
+      } catch (error) {
+        res.status[500].send('server error')
+      }
     }
 });
 
