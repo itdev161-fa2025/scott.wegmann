f13366b7f9757fe78e24f6b4da924a984317383e
scott wegmann <wegmanns@gmatc.matc.edu>
2024-11-04 08:13:06 -0600

    Completed Activity 8

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,102 +1,224 @@
 import React, { useState } from 'react';
+
 import axios from 'axios';
 
-const Register = () => {
+import { useNavigate } from 'react-router-dom';
+
+
+
+const Register = ({ authenticateUser }) => {
+
+    const navigate = useNavigate();
+
     const [userData, setUserData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        passwordConfirm: '',
+
+    name: '',
+
+    email: '',
+
+    password: '',
+
+    passwordConfirm: '',
+
     });
 
-    const {name, email,password, passwordConfirm } = userData;
+
+
+    const [errorData, setErrorData] = useState({ errors: null });
+
+    const { name, email, password, passwordConfirm } = userData;
+
+    const { errors } = errorData;
+
+
 
     const onChange = e => {
-        const { name, value } = e.target;
-        setUserData({
-            ...userData,
-            [name]: value
-        })
+
+    const { name, value } = e.target;
+
+    setUserData({
+
+    ...userData,
+
+    [name]: value
+
+    })
+
     }
 
 
-    const register = async() => {
-        if (password !== passwordConfirm) {
-            console.log('Passwords do not match');
-        }
-        else {
-            const newUser = {
-                name: name,
-                email: email,
-                password: password
-            }
 
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-                const body = JSON.stringify(newUser);
-                const res = await axios.post('http://localhost:5000/api/users', body, config);
-                console.log(res.data);
-            } catch (error) {
-                console.error(error.response.data);
-                return;
-            }
-        }
+    const registerUser = async() => {
+
+    if (password !== passwordConfirm) {
+
+        console.log('Passwords do not match');
+
     }
+
+    else {
+
+    const newUser = {
+
+    name: name,
+
+    email: email,
+
+    password: password
+
+    }
+
+
+
+    try {
+
+    const config = {
+
+        headers: {
+
+        'Content-Type': 'application/json'
+
+        }
+
+    }
+
+
+
+    const body = JSON.stringify(newUser);
+
+    const res = await axios.post('http://localhost:5000/api/users', body, config);
+
+     // Store user data and redirect
+
+    localStorage.setItem('token', res.data.token);
+
+    navigate('/');
+
+    } catch (error) {
+
+    // Clear user data and set errors
+
+    localStorage.removeItem('token');
+
+    setErrorData({
+
+            ...errors,
+
+            errors: error.response.data.errors
+
+    })
+
+    }
+
+
+
+    authenticateUser();
+
+    }
+
+    }
+
+
 
     return (
-        <div>
-            <h2>Register</h2>
-            <div>
-                <input
-                type= 'text'
-                placeholder = 'Name'
-                name = 'name'
-                value={name}
-                onChange={e => onChange(e)}
-                >
 
-                </input>
-            </div>
-                <div>
-                <input
-                type= 'text'
-                placeholder = 'Email'
-                name = 'email'
-                value={email}
-                onChange={e => onChange(e)}
-                >
-                </input>
-            </div>
-                <div>
-                <input
-                type= 'text'
-                placeholder = 'Password'
-                name = 'password'
-                value={password}
-                onChange={e => onChange(e)}
-                >
-                </input>
-            </div>
-            <div>
-                <input
-                type= 'text'
-                placeholder = 'Confirm Password'
-                name = 'passwordConfirm'
-                value={passwordConfirm}
-                onChange={e => onChange(e)}
-                >
-                </input>
-            </div>
-            <div>
-                <button onClick={() => register()}>Register</button>
-            </div>
-        </div>
-        
+    <div>
+
+    <h2>Register</h2>
+
+        <div>
+
+    <input
+
+        type='text'
+
+        placeholder='Name'
+
+        name='name'
+
+        value={name}
+
+        onChange={e => onChange(e)}
+
+    />
+
+    </div>
+
+    <div>
+
+    <input
+
+        type='text'
+
+            placeholder='Email'
+
+        name='email'
+
+        value={email}
+
+                    onChange={e => onChange(e)}
+
+    />
+
+    </div>
+
+    <div>
+
+    <input
+
+        type='text'
+
+        placeholder='Password'
+
+        name='password'
+
+        value={password}
+
+        onChange={e => onChange(e)}
+
+    />
+
+    </div>
+
+    <div>
+
+    <input
+
+        type='text'
+
+        placeholder='Confirm Password'
+
+        name='passwordConfirm'
+
+        value={passwordConfirm}
+
+        onChange={e => onChange(e)}
+
+    />
+
+    </div>
+
+    <div>
+
+    <button onClick={() => registerUser()}>Register</button>
+
+    </div>
+
+    <div>
+
+    {errors && errors.map(error => 
+
+        <div key={error.msg}>{error.msg}</div>)}
+
+    </div>
+
+    </div>
+
     )
+
 }
 
-export default Register;
+
+
+export default Register
+
